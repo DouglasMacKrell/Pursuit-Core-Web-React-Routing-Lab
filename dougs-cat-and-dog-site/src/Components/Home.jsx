6db4636b7ee0edd8e1b97a8c50dfe2aa4ffc56b3
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-01-25 23:01:07 -0500

    randomdog updated to hooks

--- dougs-cat-and-dog-site/src/Components/Home.jsx
+++ dougs-cat-and-dog-site/src/Components/Home.jsx
@@ -1,21 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Home extends Component {
-    constructor() {
-        super();
-        this.state = {
+const Home = () => {
 
-        }
-    }
 
-    render() {
-        return (
-            <div>
-                <h1>Welcome To Doug's Cat & Dog Page!</h1>
-                <img id="bunsen" src="/Bunsen-Window.jpg" alt="my cat"></img>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h1>Welcome To Doug's Cat & Dog Page!</h1>
+            <img id="bunsen" src="/Bunsen-Window.jpg" alt="my cat"></img>
+        </div>
+    )
+
 }
 
 export default Home
--- dougs-cat-and-dog-site/src/Components/RandomDog.jsx
+++ dougs-cat-and-dog-site/src/Components/RandomDog.jsx
@@ -1,49 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-class RandomDog extends Component {
-    constructor() {
-        super();
-        this.state = {
-            url: ""
-        }
+const RandomDog = () => {
+
+    const [url, setUrl] = useState('');
+
+    
+    const getDogPictures = async () => {
+        const response = await axios.get(`https://dog.ceo/api/breeds/image/random`)
+        setUrl(response.data.message)
+        console.log(response.data.message)
     }
+    
+    useEffect(() => {
+        getDogPictures()
+    }, [])
+    
+    console.log(url)
 
+    return (
+        <div>
+            <h1>One Random Dog!</h1>
+            <img src={url} alt="Good Dog"></img>
+        </div>
+    )
 
-    getDogPictures = async () => {
-        let dogAPIURL = `https://dog.ceo/api/breeds/image/random`
-
-        try {
-            const { data } = await axios.get(dogAPIURL)
-            console.log(data)
-            this.setState({
-                url: data.message,
-            })
-
-        } catch (error) {
-            console.log('err: ', error)
-        }
-
-    }
-
-    handleNewDogBtn = () => {
-        this.getDogPictures();
-    }
-
-    componentDidMount() {
-        this.getDogPictures();
-    }
-
-    render() {
-        const { url } = this.state;
-
-        return (
-            <div>
-                <h1>One Random Dog!</h1>
-                <img src={url} alt="Good Dog"></img>
-            </div>
-        )
-    }
 }
 
 export default RandomDog
